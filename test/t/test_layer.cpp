5b85eb98b6eb9f764eabc5e6faa1bb769bf95765
Jochen Topf <jochen@topf.org>
2018-06-22 14:17:25 +0200

    Test layer with and without compression.

--- test/t/test_layer.cpp
+++ test/t/test_layer.cpp
@@ -16,10 +16,20 @@ TEST_CASE("Encode and decode layer") {
     const char* name = "test";
     const char* content = "the quick brown fox jumps over the lazy dog";
 
+    tgd_header::layer_compression_type ct;
+
+    SECTION("without compression") {
+        ct = tgd_header::layer_compression_type::uncompressed;
+    }
+
+    SECTION("with compression") {
+        ct = tgd_header::layer_compression_type::zlib;
+    }
+
     // set up layer
     tgd_header::layer layer;
     layer.set_content_type(tgd_header::layer_content_type::unknown);
-    layer.set_compression_type(tgd_header::layer_compression_type::uncompressed);
+    layer.set_compression_type(ct);
     layer.set_tile(tgd_header::tile_address{4, 3, 2});
     layer.set_name(name);
     layer.set_content(content, std::strlen(content) + 1);
@@ -41,7 +51,7 @@ TEST_CASE("Encode and decode layer") {
 
     REQUIRE(new_layer);
     REQUIRE(new_layer.content_type() == tgd_header::layer_content_type::unknown);
-    REQUIRE(new_layer.compression_type() == tgd_header::layer_compression_type::uncompressed);
+    REQUIRE(new_layer.compression_type() == ct);
     REQUIRE(new_layer.tile() == tgd_header::tile_address(4, 3, 2));
     REQUIRE(new_layer.name_length() == std::strlen(name) + 1);
     REQUIRE(!std::strncmp(new_layer.name().data(), name, std::strlen(name)));
