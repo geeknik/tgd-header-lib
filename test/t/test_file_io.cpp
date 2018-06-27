dc29178d4ff8cb8da649bc343ae43df90fda9095
Jochen Topf <jochen@topf.org>
2018-06-27 17:39:37 +0200

    Test various file io failure cases.

--- test/t/test_file_io.cpp
+++ test/t/test_file_io.cpp
@@ -35,8 +35,12 @@ TEST_CASE("Write and read buffer") {
     REQUIRE(source.file_size() == data_size);
 
     const auto in_buffer = source.read(data_size);
+    REQUIRE(in_buffer);
     REQUIRE(in_buffer.size() == out_buffer.size());
     REQUIRE(std::equal(in_buffer.begin(), in_buffer.end(), out_buffer.begin()));
+
+    const auto end_buffer = source.read(1);
+    REQUIRE_FALSE(end_buffer);
 
     unlink(filename);
 }
@@ -94,6 +98,8 @@ TEST_CASE("Write and read buffer using mmap with padding and skip") {
     tgd_header::mmap_source source{filename};
     REQUIRE(source.file_size() == data_size + padding_size);
 
+    REQUIRE_THROWS_AS(source.skip(1000), const std::range_error&);
+
     source.skip(padding_size);
 
     SECTION("move construct/swap source") {
@@ -102,9 +108,18 @@ TEST_CASE("Write and read buffer using mmap with padding and skip") {
         swap(source2, source);
     }
 
+    SECTION("move construct/move assign source") {
+        tgd_header::mmap_source source2{std::move(source)};
+        source = std::move(source2);
+    }
+
     const auto in_buffer = source.read(data_size);
+    REQUIRE(in_buffer);
     REQUIRE(in_buffer.size() == out_buffer.size());
     REQUIRE(std::equal(in_buffer.begin(), in_buffer.end(), out_buffer.begin()));
+
+    const auto end_buffer = source.read(1);
+    REQUIRE_FALSE(end_buffer);
 
     unlink(filename);
 }
@@ -119,13 +134,88 @@ TEST_CASE("File source stdin (-)") {
     REQUIRE(source.fd() == 0);
 }
 
-TEST_CASE("File sink stdin (empty)") {
+TEST_CASE("File sink stdout (empty)") {
     tgd_header::file_sink sink{""};
     REQUIRE(sink.fd() == 1);
 }
 
-TEST_CASE("File sink stdin (-)") {
+TEST_CASE("File sink stdout (-)") {
     tgd_header::file_sink sink{"-"};
     REQUIRE(sink.fd() == 1);
+}
+
+TEST_CASE("Calling close() on closed file") {
+    const auto filename = "test_file_4";
+
+    tgd_header::file_sink sink{filename};
+    close(sink.fd()); // you should never do this
+    REQUIRE_THROWS_AS(sink.close(), const std::system_error&);
+
+    unlink(filename);
+}
+
+TEST_CASE("Destructor must not throw") {
+    const auto filename = "test_file_4";
+
+    {
+        tgd_header::file_sink sink{filename};
+        close(sink.fd()); // you should never do this
+    }
+    // destructor of sink called here, must not throw
+
+    unlink(filename);
+}
+
+TEST_CASE("Calling file_size() on closed file") {
+    const auto filename = "test_file_5";
+
+    tgd_header::file_sink sink{filename};
+    close(sink.fd()); // you should never do this
+    REQUIRE_THROWS_AS(sink.file_size(), const std::system_error&);
+
+    unlink(filename);
+}
+
+TEST_CASE("Throw when trying to open nonexistent file") {
+    const auto filename = "file_does_not_exist";
+
+    REQUIRE_THROWS_AS(tgd_header::file_source{filename}, const std::system_error&);
+}
+
+TEST_CASE("Throw when there is an error writing to a file") {
+    const auto filename = "test_file_6";
+
+    tgd_header::file_sink sink{filename};
+    sink.padding(2);
+    close(sink.fd()); // you should never do this
+    REQUIRE_THROWS_AS(sink.padding(2), const std::system_error&);
+
+    unlink(filename);
+}
+
+TEST_CASE("Throw when there is an error seeking in a file") {
+    const auto filename = "test_file_7";
+
+    tgd_header::file_sink sink{filename};
+    sink.padding(5);
+    sink.close();
+
+    tgd_header::file_source source{filename};
+    close(source.fd()); // you should never do this
+    REQUIRE_THROWS_AS(source.skip(5), const std::system_error&);
+
+    unlink(filename);
+}
+
+TEST_CASE("Mmapping zero-length file fails") {
+    const auto filename = "test_file_8";
+
+    tgd_header::file_sink sink{filename};
+    sink.close();
+
+    // XXX this should fail (length is zero), but doesn't for some reason
+    // tgd_header::mmap_source source{filename};
+
+    unlink(filename);
 }
 
