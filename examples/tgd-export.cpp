8c2950d543c0b30ad406ba7984c433aaa356753e
Jochen Topf <jochen@topf.org>
2018-06-28 14:57:11 +0200

    Make export example command work with and without layer.

--- examples/tgd-export.cpp
+++ examples/tgd-export.cpp
@@ -2,14 +2,17 @@
 
   tgd-export
 
-  Export the content of a layer from a tile file.
+  Export the content of the first layer from a tile file. If a layer name
+  was specified the first layer of that name ies exported.
 
   Reads from the specified input file and writes to stdout. If the layer
   doesn't exist, nothing is written.
 
   Examples:
 
-  tgd-export input.tgd buildings >buildings.layer
+  tgd-export input.tgd buildings -o buildings.layer
+
+  tgd-export input.tgd -o first.layer
 
 *****************************************************************************/
 
@@ -19,31 +22,60 @@
 #include <tgd_header/layer.hpp>
 #include <tgd_header/reader.hpp>
 
+#include <clara.hpp>
+
 #include <iostream>
 #include <string>
 
-void print_usage() {
-    std::cout << "tgd-export FILE LAYER\n";
-    std::cout << "Write content of layer LAYER in the FILE to stdout.\n";
-}
-
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        print_usage();
+    std::string input_file_name;
+    std::string output_file_name;
+    std::string layer_name;
+    bool help = false;
+
+    const auto cli
+        = clara::Opt(layer_name, "layer name")
+            ["-l"]["--layer"]
+            ("layer to export (default: first layer)")
+        | clara::Opt(output_file_name, "file")
+            ["-o"]["--output"]
+            ("output file (default: stdout)")
+        | clara::Help(help)
+        | clara::Arg(input_file_name, "FILE")
+            ("data");
+
+    const auto result = cli.parse(clara::Args(argc, argv));
+    if (!result) {
+        std::cerr << "Error in command line: " << result.errorMessage() << '\n';
         return 2;
     }
 
-    std::string name{argv[2]};
+    if (help) {
+        std::cout << "Export layer content.\n\n";
+        std::cout << cli;
+        return 0;
+    }
 
-    tgd_header::file_source source{argv[1]};
+    if (input_file_name.empty()) {
+        std::cerr << "Missing input file. Try 'tgd-export -h'.\n";
+        return 2;
+    }
+
+    if (output_file_name.empty()) {
+        std::cerr << "Missing -o/--output option. Try 'tgd-export -h'.\n";
+        return 2;
+    }
+
+    tgd_header::file_source source{input_file_name};
     tgd_header::reader<decltype(source)> reader{source};
 
     while (auto& layer = reader.next_layer()) {
-        if (layer.name().data() == name) {
+        if (layer_name.empty() || layer.name().data() == layer_name) {
             reader.read_content();
             layer.decode_content();
-            tgd_header::file_sink out{"-"};
+            tgd_header::file_sink out{output_file_name};
             out.write(layer.content());
+            return 0;
         }
     }
 
